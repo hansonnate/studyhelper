5ea242c6f54f707659e97101e7d98de1c04994dd
Nate Hanson <hansonnate7@gmail.com>
2021-11-07 23:00:21 -0700

    mock data upload

--- helper/src/mock-data.js
+++ helper/src/mock-data.js
@@ -12,6 +12,34 @@ let mock = [{
     verse: "27",
     content: "And if men come unto me I will show unto them their weakness. I give unto men weakness that they may be humble; and my grace is sufficient for all men that humble themselves before me; for if they humble themselves before me, and have faith in me, then will I make weak things become strong unto them."
   },
+  {
+    id: 3,
+    book: "2 Nephi",
+    chapter: "2",
+    verse: "25",
+    content: "Adam fell that men might be; and men care, that they might have joy."
+  },
+  {
+    id: 4,
+    book: "2 Nephi",
+    chapter: "31",
+    verse: "20",
+    content: "Wherefore, ye must press forward with a steadfastness in Christ, having a perfect brightness of hope, and a clove of God and of all men. Wherefore, if ye shall press forward, feasting upon the word of Christ, and endure to the end, behold, thus saith the Father: Ye shall have eternal life."
+  },
+  {
+    id: 5,
+    book: "Mosiah",
+    chapter: "2",
+    verse: "17",
+    content: "And behold, I tell you these things that ye may learn wisdom; that ye may learn that when ye are in the service of your fellow beings ye are only in the service of your God."
+  },
+  {
+    id: 6,
+    book: "Helaman",
+    chapter: "5",
+    verse: "12",
+    content: "And now, my sons, remember, remember that it is upon the rock of our Redeemer, who is Christ, the Son of God, that ye must build your bfoundation; that when the devil shall send forth his mighty winds, yea, his shafts in the whirlwind, yea, when all his hail and his mighty cstorm shall beat upon you, it shall have no power over you to drag you down to the gulf of misery and endless wo, because of the rock upon which ye are built, which is a sure foundation, a foundation whereon if men build they cannot fall."
+  },
 
 ]
 
